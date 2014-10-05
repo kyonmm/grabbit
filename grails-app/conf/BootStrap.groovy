3f0c22ec8881fc035bcca8b6eed86a422f614fca
kyon_mm <kyon.mm@gmail.com>
2014-10-06 04:26:54 +0900

    delete buildpack

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -7,27 +7,28 @@ import org.kyonmm.grabbit.TestCase
 class BootStrap {
 
     def init = { servletContext ->
-// ユーザの登録
-        def testUser = new SecureUser(
-                username: "kyonmm",
-                password: "password",
-        ).save(flush: true)
+        if(!SecureUser.findByUsername("kyonmm")){
+            def testUser = new SecureUser(
+                    username: "kyonmm",
+                    password: "password",
+            ).save(flush: true)
+            def userRole = new SecureRole(authority: 'ROLE_USER').save()
+            def adminRole = new SecureRole(authority: 'ROLE_ADMIN').save()
 
-// ロールの登録
-        def userRole = new SecureRole(authority: 'ROLE_USER').save()
-        def adminRole = new SecureRole(authority: 'ROLE_ADMIN').save()
+            SecureUserSecureRole.create(testUser, adminRole).save()
+        }
 
-        SecureUserSecureRole.create(testUser, adminRole).save()
+        if(Tag.count == 0){
+            def useCase = new Tag(name: "ユースケース", description: "エンドユーザーとシステムとのインタラクションを明示的にしたもの").save()
+            new Tag(name: "スケーラビリティ", description: "システムのスケーラビリティを明示的にしたもの").save()
+            new Tag(name: "スムーズさ", description: "システムのなめらかさを明示的にしたもの").save()
+            new Tag(name: "システムレベルのDI", description: "システムの挙動を設定ファイルで変更することを明示的にしたもの").save()
+            new Tag(name: "追跡性", description: "システムの過去の挙動に関する立証性を明示的にしたもの").save()
+            new Tag(name: "学習曲線", description: "システムを理解し、習得していく流れを明示的にしたもの").save()
 
-        def useCase = new Tag(name: "ユースケース", description: "エンドユーザーとシステムとのインタラクションを明示的にしたもの").save()
-        new Tag(name: "スケーラビリティ", description: "システムのスケーラビリティを明示的にしたもの").save()
-        new Tag(name: "スムーズさ", description: "システムのなめらかさを明示的にしたもの").save()
-        new Tag(name: "システムレベルのDI", description: "システムの挙動を設定ファイルで変更することを明示的にしたもの").save()
-        new Tag(name: "追跡性", description: "システムの過去の挙動に関する立証性を明示的にしたもの").save()
-        new Tag(name: "学習曲線", description: "システムを理解し、習得していく流れを明示的にしたもの").save()
-
-        new TestCase(name: "テストケースにはタグがひもづいている", tags: [useCase],
-                scenario: "テストケースには登録されているタグが0から任意の個数まで重複なく登録されている。").save()
+            new TestCase(name: "テストケースにはタグがひもづいている", tags: [useCase],
+                    scenario: "テストケースには登録されているタグが0から任意の個数まで重複なく登録されている。").save()
+        }
     }
     def destroy = {
     }
